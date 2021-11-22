bf3b0c9039a12e6165d21f3c956e33e70dea855a
Or Finberg <orff1989@walla.com>
2021-11-22 23:54:58 +0200

    v3

--- main.c
+++ main.c
@@ -3,14 +3,16 @@
 #include "my_mat.h"
 
 int main(){
-    char ch='x';
+char ch='x';
     
 while (ch != 'D')
 {
    scanf("%c",&ch);
-   if (ch=='A') function1(); 
-   if (ch=='B') function2(); 
-   if (ch=='C') function3(); 
+   if (ch=='A') function1();
+    
+    
+   //if (ch=='B') function2(); 
+   //if (ch=='C') function3(); 
 }
 
     return 0;
--- my_mat.c
+++ my_mat.c
@@ -11,33 +11,38 @@ void function1(){
                scanf("%i",&num);
                mat[i][j]=num; //shoud be a char or int?
            }
-}
-int[] FWalgorithm(int i, int j){
-   return new int[]; 
+        }
 }
 
-void function2(){
-    int i;
-    int j;
-    scanf("%d",&i);
-    scanf("%d",&j);
-    int[] path =FWalgorithm(i,j);
-    if (sizeof(path)==0) printf("False")
-    else printf("True")
-}
 
-void function3(){
-    int i;
-    int j;
-    scanf("%d",&i);
-    scanf("%d",&j);
 
-    int[] path =FWalgorithm(i,j);
-    sizeOfPath= sizeof(path)/ sizeof(int);
-    if (sizeOfPath==0) printf("-1");
+// int* FWalgorithm(int i, int j){
+//     int arr[10];
+//    return arr; 
+// }
 
-    for (int k = 0; k < sizeOfPath; k++)
-    {
-        printf("%d",path[k]);
-    }
-}
+// void function2(){
+//     int i;
+//     int j;
+//     scanf("%d",&i);
+//     scanf("%d",&j);
+//     int[] path =FWalgorithm(i,j);
+//     if (sizeof(path)==0) printf("False")
+//     else printf("True")
+// }
+
+// void function3(){
+//     int i;
+//     int j;
+//     scanf("%d",&i);
+//     scanf("%d",&j);
+
+//     int[] path =FWalgorithm(i,j);
+//     sizeOfPath= sizeof(path)/ sizeof(int);
+//     if (sizeOfPath==0) printf("-1");
+
+//     for (int k = 0; k < sizeOfPath; k++)
+//     {
+//         printf("%d",path[k]);
+//     }
+// }
